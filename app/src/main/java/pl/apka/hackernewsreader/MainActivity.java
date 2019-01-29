793032407cbb9bbc54a4a0869d7b2de9ff1550f8
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-01-29 16:35:58 +0100

    download task

--- app/src/main/java/pl/apka/hackernewsreader/MainActivity.java
+++ app/src/main/java/pl/apka/hackernewsreader/MainActivity.java
@@ -3,9 +3,12 @@ package pl.apka.hackernewsreader;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -32,12 +35,37 @@ public class MainActivity extends AppCompatActivity {
     public class DownloadTask extends AsyncTask<String, Void, String>{
 
         @Override
-        protected String doInBackground(String... strings) {
+        protected String doInBackground(String... urls) {
 
             String result = "";
             URL url;
             HttpURLConnection urlConnection = null;
-            
+
+            try {
+
+                url = new URL(urls[0]);
+                urlConnection =  (HttpURLConnection) url.openConnection();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int data = inputStreamReader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+
+                Log.i("URL CONTENT: ",result);
+
+                return result;
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
             return null;
         }
     }
