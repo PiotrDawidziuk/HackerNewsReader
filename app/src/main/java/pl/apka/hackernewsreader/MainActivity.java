59230e600fa3807ec6ceb7a45dea3f9649f1b674
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-01-29 16:59:29 +0100

    json link

--- app/src/main/java/pl/apka/hackernewsreader/MainActivity.java
+++ app/src/main/java/pl/apka/hackernewsreader/MainActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -24,10 +26,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DownloadTask task = new DownloadTask();
+        try {
+            task.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+        }catch (Exception e) {
+
+        }
+
+
         ListView listView = findViewById(R.id.listView);
-
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
-
         listView.setAdapter(arrayAdapter);
 
     }
@@ -57,10 +65,39 @@ public class MainActivity extends AppCompatActivity {
                     data = inputStreamReader.read();
                 }
 
+                JSONArray jsonArray = new JSONArray(result);
+
+                int numberOfItems = 20;
+
+                if (jsonArray.length()<20){
+                    numberOfItems = jsonArray.length();
+                }
+
+                for (int i =0;  i <numberOfItems; i++) {
+                    String articleId = jsonArray.getString(i);
+                    url = new URL( "https://hacker-news.firebaseio.com/v0/item/" + articleId + ".json?print=pretty");
+
+                    urlConnection =  (HttpURLConnection) url.openConnection();
+
+                    inputStream = urlConnection.getInputStream();
+                    inputStreamReader = new InputStreamReader(inputStream);
+
+                    data = inputStreamReader.read();
+
+                    String articleInfo ="";
+
+                    while (data != -1) {
+                        char current = (char) data;
+                        articleInfo += current;
+                        data = inputStreamReader.read();
+                    }
+                    Log.i("Article Info",articleInfo);
+                }
+
                 Log.i("URL CONTENT: ",result);
 
                 return result;
-                
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
